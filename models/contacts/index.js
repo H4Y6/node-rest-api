01c980d1cb0baa31fa3cb03f4d433de7d0197791
H4Y6 <h4y6@i.ua>
2023-01-22 10:57:06 +0200

    modified:   models/contacts/index.js,   routes/api/contacts.js

--- models/contacts/index.js
+++ models/contacts/index.js
@@ -1,3 +1,59 @@
-const contacts = require("./contacts.json");
+// const fs = require("fs/promises");
+// const path = require("path");
+// const { nanoid } = require("nanoid");
+// const contactsPath = path.join(__dirname, "contacts.json");
 
-module.exports = contacts;
+// const updateContacts = async (contacts) => {
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+// };
+
+// const listContacts = async () => {
+//   const data = await fs.readFile(contactsPath);
+//   return JSON.parse(data);
+// };
+
+// const getContactById = async (contactId) => {
+//   const contacts = await listContacts();
+//   const idContact = contacts.find((c) => c.id === contactId);
+//   return idContact ? idContact : null;
+// };
+
+// const removeContact = async (contactId) => {
+//   const contacts = await listContacts();
+//   const idx = contacts.findIndex((c) => c.id === contactId);
+//   if (idx !== -1) {
+//     const [removedContact] = contacts.splice(idx, 1);
+//     await updateContacts(contacts);
+//     return removedContact;
+//   }
+//   return null;
+// };
+
+// const addContact = async (name, email, phone) => {
+//   const contacts = await listContacts();
+//   const newContact = { id: nanoid(), name, email, phone };
+//   contacts.push(newContact);
+//   await updateContacts(contacts);
+//   return newContact;
+// };
+
+// module.exports = { listContacts, getContactById, removeContact, addContact };
+
+const fs = require("fs/promises");
+const path = require("path");
+const nanoid = require("nanoid");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  const list = await fs.readFile(contactsPath);
+  return JSON.parse(list);
+};
+
+const getById = async (id) => {
+  const contacts = await listContacts();
+  const idContact = contacts.find((c) => c.id === id);
+  return idContact ? idContact : null;
+};
+
+module.exports = { listContacts, getById };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,4 +4,26 @@ const contacts = require("../../models/contacts");
 
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getById(id);
+    if (!result) {
+      return res.status(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
