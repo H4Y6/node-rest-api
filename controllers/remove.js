ec96f8f0a8f3154f47afa8ca89ca76a59f663498
H4Y6 <h4y6@i.ua>
2023-03-29 21:16:04 +0300

    modified:   controllers/getById.js modified:   controllers/remove.js modified:   controllers/updateById.js modified:   controllers/updateStatus.js

--- controllers/remove.js
+++ controllers/remove.js
@@ -2,16 +2,12 @@ const { Contact } = require("../models");
 const { createError } = require("../helpers");
 
 const remove = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Contact.findByIdAndDelete(id);
-    if (!result) {
-      throw createError(404);
-    }
-    res.json("Contact deleted");
-  } catch (error) {
-    next(error);
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    throw createError(404);
   }
+  res.json("Contact deleted");
 };
 
 module.exports = remove;
--- controllers/updateById.js
+++ controllers/updateById.js
@@ -2,23 +2,19 @@ const { Contact, schemas } = require("../models");
 const { createError } = require("../helpers");
 
 const updateById = async (req, res, next) => {
-  try {
-    const { error } = schemas.add.validate(req.body);
-    if (error) {
-      throw createError(400, error.message);
-    }
-    const { id } = req.params;
-
-    const result = await Contact.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+  const { error } = schemas.add.validate(req.body);
+  if (error) {
+    throw createError(400, error.message);
   }
+  const { id } = req.params;
+
+  const result = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw createError(404);
+  }
+  res.json(result);
 };
 
 module.exports = updateById;
