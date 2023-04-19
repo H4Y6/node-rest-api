a162d3f53bafed67bcf11b1764b546166bc687fe
H4Y6 <h4y6@i.ua>
2023-04-19 19:43:50 +0300

    new files: middlewares/auth.js,middlewares/index.js 	modified: models/contacts.js, controllers/contacts/add.j, routes/api/contacts.js

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const { basedir } = global;
+const { User } = require(`${basedir}/models/users`);
+const { createError } = require(`${basedir}/helpers`);
+const { SECRET_KEY } = process.env;
+
+const auth = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (!bearer) {
+    next(createError(401));
+  }
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(createError(401));
+  }
+};
+
+module.exports = auth;
