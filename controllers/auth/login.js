f48b750f501ba8dfdc079e04465b64012b8f4bea
H4Y6 <h4y6@i.ua>
2023-04-19 18:17:15 +0300

    new file:   controllers/auth/login.js modified:   controllers/auth/index.js

---
+++ controllers/auth/login.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { basedir } = global;
+const { User, schemas } = require(`${basedir}/models/users`);
+const { createError } = require(`${basedir}/helpers`);
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+  const { error } = schemas.login.validate(req.body);
+  if (error) {
+    throw createError(400, error.message);
+  }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !user.token) {
+    throw createError(401, "Wrong email");
+  }
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    throw createError(401, "Wrong password.");
+  }
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
+  const result = await User.findByIdAndUpdate(user._id, { token });
+  const { subscription } = result;
+  res.json({
+    token,
+    user: { email, subscription },
+  });
+};
+
+module.exports = login;
