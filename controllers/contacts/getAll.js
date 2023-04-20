71bb25228bbb08ed70ca1a06c963ab017f870140
H4Y6 <h4y6@i.ua>
2023-04-20 15:07:51 +0300

    modified:   app.js modified:   controllers/contacts/getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,10 +3,17 @@ const { Contact } = require(`${basedir}/models/contacts`);
 
 const getAll = async (req, res, next) => {
   try {
+    const { page = 1, limit = 20, favorite = false } = req.query;
     const { id: owner } = req.user;
+    const skip = (page - 1) * limit;
     const result = await Contact.find(
-      { owner },
-      "-createdAt -updatedAt"
+      { owner, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit: Number(limit),
+        favorite,
+      }
     ).populate("owner", "email subscription");
     res.json(result);
   } catch (error) {
