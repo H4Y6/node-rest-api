81ba1b4e67751d495496589d5c3026f0359ea9de
H4Y6 <h4y6@i.ua>
2023-04-28 17:39:42 +0300

    modified:   controllers/auth/register.js 	modified:   middlewares/index.js 	new file:   middlewares/upload.js 	modified:   routes/api/auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { basedir } = global;
 const ctrl = require(`${basedir}/controllers/auth`);
 const { ctrlWrapper } = require(`${basedir}/helpers`);
-const { auth } = require(`${basedir}/middlewares`);
+const { auth, upload } = require(`${basedir}/middlewares`);
 
 const router = express.Router();
 
@@ -12,5 +12,11 @@ router.post("/login", ctrlWrapper(ctrl.login));
 router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 router.patch("/", auth, ctrlWrapper(ctrl.updateSubscription));
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.setAvatar)
+);
 
 module.exports = router;
